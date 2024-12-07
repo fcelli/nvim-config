df5ced9268d77688746c1c9f9bde082abdea833a
fcelli <fedecel93@gmail.com>
2024-12-07 21:51:11 +0000

    feat: status line

---
+++ lua/fcelli/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+  -- Collection of various small independent plugins/modules
+  "echasnovski/mini.nvim",
+  config = function()
+    -- Simple and easy statusline.
+    local statusline = require "mini.statusline"
+    statusline.setup {}
+  end
+}
